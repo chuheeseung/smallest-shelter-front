4fb8a74df7593657ab9937896062504ed69e031b
chuheeseung <youverygood@naver.com>
2022-10-24 15:28:43 +0900

    22.10.24

--- the-smallest-shelter/src/components/ListviewPage/PageButtons.jsx
+++ the-smallest-shelter/src/components/ListviewPage/PageButtons.jsx
@@ -5,97 +5,132 @@ import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { ButtonWrap, PageButton } from '../MyPage/MyLikeAnimal';
 import { useState } from 'react';
+import { PageButtonNum } from '../../states/PageState';
 
-export default function PageButtons({ handleCardList, click, pageNum, maxPageNum, data }) {
-  const handlePrevious = async () => {
-    if(click === true) {
-      console.log(1);
-      await axios
-        .post(
-          `https://sjs.hana-umc.shop/animal/search?page=${pageNum}`,
-          JSON.stringify(data),
-          {
-            headers: {
-              'Content-Type': `application/json`,
-            },
-          },
-          {
-            params: { page: (pageNum > 0 ? pageNum - 1 : 0) },
-          }
-        )
-        .then((res) => {
-          console.log(res.data.result);
-          handleCardList(res.data.result.animal);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    else if (click === false) {
-      console.log(2);
-      await axios.get("https://sjs.hana-umc.shop/animals",
-          {params: {page: (pageNum > 0 ? pageNum - 1 : 0)}},
-          {withCredentials: true}
-      ).then((res) => {
-          console.log(res.data.result);
-          handleCardList(res.data.result.animal);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
-  };
+export default function PageButtons({
+	handleCardList,
+	click,
+	pageNum,
+	maxPageNum,
+	data,
+}) {
+	const [pageNumber, setPageNumber] = useRecoilState(PageButtonNum);
 
-  const handleNext = async () => {
-    if(click === true) {
-      console.log(3);
-      console.log(maxPageNum);
-      await axios
-        .post(
-          `https://sjs.hana-umc.shop/animal/search?page=${pageNum}`,
-          JSON.stringify(data),
-          {
-            headers: {
-              'Content-Type': `application/json`,
-            },
-          },
-          {
-            params: { page: (pageNum < maxPageNum ? pageNum + 1 : maxPageNum) },
-          }
-        ).then((res) => {
-          console.log(res.data.result);
-          handleCardList(res.data.result.animal);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    else if (click === false) {
-      console.log(4);
-      await axios
-        .get(
-          'https://sjs.hana-umc.shop/animals',
-          { params: { page: (pageNum < maxPageNum ? pageNum + 1 : maxPageNum) } },
-          { withCredentials: true }
-        )
-        .then((res) => {
-          console.log(res.data.result);
-          handleCardList(res.data.result.animal);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  };
+	const handlePrevious = async () => {
+		if (click === true) {
+			console.log(1);
+			await axios
+				.post(
+					`https://sjs.hana-umc.shop/animal/search?page=${pageNum}`,
+					JSON.stringify(data),
+					{
+						headers: {
+							'Content-Type': `application/json`,
+						},
+					},
+					{
+						params: { page: pageNum > 0 ? pageNum - 1 : 0 },
+					}
+				)
+				.then((res) => {
+					console.log(res.data.result);
+					handleCardList(res.data.result.animal);
 
-  useEffect(() => {
-    console.log(maxPageNum);
-  }, []);
+					if (pageNum > 0) {
+						setPageNumber(pageNum - 1);
+					} else {
+						setPageNumber(0);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else if (click === false) {
+			console.log(2);
+			await axios
+				.get(
+					'https://sjs.hana-umc.shop/animals',
+					{ params: { page: pageNum > 0 ? pageNum - 1 : 0 } },
+					{ withCredentials: true }
+				)
+				.then((res) => {
+					console.log(res.data.result);
+					handleCardList(res.data.result.animal);
 
-  return (
-    <ButtonWrap>
-      <PageButton onClick={handlePrevious}>이전</PageButton>
-      <PageButton onClick={handleNext}>다음</PageButton>
-    </ButtonWrap> 
-  )
+					if (pageNum > 0) {
+						setPageNumber(pageNum - 1);
+					} else {
+						setPageNumber(0);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
+
+	const handleNext = async () => {
+		if (click === true) {
+			console.log(3);
+			console.log(maxPageNum);
+			await axios
+				.post(
+					`https://sjs.hana-umc.shop/animal/search?page=${pageNum}`,
+					JSON.stringify(data),
+					{
+						headers: {
+							'Content-Type': `application/json`,
+						},
+					},
+					{
+						params: { page: pageNum < maxPageNum ? pageNum + 1 : maxPageNum },
+					}
+				)
+				.then((res) => {
+					console.log(res.data.result);
+					handleCardList(res.data.result.animal);
+
+					if (pageNum < maxPageNum) {
+						setPageNumber(pageNum + 1);
+					} else {
+						setPageNumber(maxPageNum);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else if (click === false) {
+			console.log(4);
+			await axios
+				.get(
+					'https://sjs.hana-umc.shop/animals',
+					{ params: { page: pageNum < maxPageNum ? pageNum + 1 : maxPageNum } },
+					{ withCredentials: true }
+				)
+				.then((res) => {
+					console.log(res.data.result);
+					handleCardList(res.data.result.animal);
+
+					if (pageNum < maxPageNum) {
+						setPageNumber(pageNum + 1);
+					} else {
+						setPageNumber(maxPageNum);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
+
+	useEffect(() => {
+		console.log(maxPageNum);
+	}, []);
+
+	return (
+		<ButtonWrap>
+			<PageButton onClick={handlePrevious}>이전</PageButton>
+			<PageButton onClick={handleNext}>다음</PageButton>
+		</ButtonWrap>
+	);
 }
--- the-smallest-shelter/src/routes/ListviewScreen.js
+++ the-smallest-shelter/src/routes/ListviewScreen.js
@@ -8,102 +8,104 @@ import axios from 'axios';
 import { useRecoilState } from 'recoil';
 import { LoginUserToken, LoginRole, LoginUserIdx } from '../states/LoginState';
 import PageButtons from '../components/ListviewPage/PageButtons';
+import { PageButtonNum } from '../states/PageState';
 
 export default function ListviewScreen() {
-  const [userToken, setUserToken] = useRecoilState(LoginUserToken);
-  const [cardList, setCardList] = useState([]); 
-  const [pageNum, setPageNum] = useState(0);
+	const [userToken, setUserToken] = useRecoilState(LoginUserToken);
+	const [cardList, setCardList] = useState([]);
+	// const [pageNum, setPageNum] = useState(0);
+	const [pageNum, setPageNum] = useRecoilState(PageButtonNum);
 
-  const [click, setClick] = useState(false); 
-  const [maxPageNum, setMaxPageNum] = useState(1); 
-  const [data, setData] = useState({
-    species: "",
-    gender: "",
-    ageBoundary: "",
-    isAdopted: "",
-  });
+	const [click, setClick] = useState(false);
+	const [maxPageNum, setMaxPageNum] = useState(1);
+	const [data, setData] = useState({
+		species: '',
+		gender: '',
+		ageBoundary: '',
+		isAdopted: '',
+	});
 
-  const handleFilter = async (filters) => {
-    let data = {
-      species: filters['species'],
-      gender: filters['gender'],
-      ageBoundary: filters['age'],
-      isAdopted: filters['isAdopted'],
-    };
-    
-    setData({
-      ...data,
-      species: filters['species'],
-      gender: filters['gender'],
-      ageBoundary: filters['age'],
-      isAdopted: filters['isAdopted'],
-    });
+	const handleFilter = async (filters) => {
+		let data = {
+			species: filters['species'],
+			gender: filters['gender'],
+			ageBoundary: filters['age'],
+			isAdopted: filters['isAdopted'],
+		};
 
-    await axios
-      .post(
-        `https://sjs.hana-umc.shop/animal/search?page=${pageNum}`,
-        JSON.stringify(data),
-        {
-          headers: {
-            'Content-Type': `application/json`,
-          },
-        },
-        {
-          params: { page: pageNum },
-        }
-      )
-      .then((response) => {
-        setCardList(response.data.result.animal);
-        setPageNum(pageNum);
-        setMaxPageNum(response.data.result.pageNumber);
-        setClick(true);
-        
-        console.log(response);
-        console.log(click);
-      });
-  };
+		setData({
+			...data,
+			species: filters['species'],
+			gender: filters['gender'],
+			ageBoundary: filters['age'],
+			isAdopted: filters['isAdopted'],
+		});
 
-  const handleCardList = (data) => {
-    setCardList(data);
-  };
+		await axios
+			.post(
+				`https://sjs.hana-umc.shop/animal/search?page=${pageNum}`,
+				JSON.stringify(data),
+				{
+					headers: {
+						'Content-Type': `application/json`,
+					},
+				},
+				{
+					params: { page: pageNum },
+				}
+			)
+			.then((response) => {
+				setCardList(response.data.result.animal);
+				setPageNum(pageNum);
+				setMaxPageNum(response.data.result.pageNumber);
+				setClick(true);
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://sjs.hana-umc.shop/animals',
-        { params: { page: pageNum } },
-        { withCredentials: true }
-      )
-      .then((res) => {
-        console.log(res.data.result);
-        setCardList(res.data.result.animal);
-        console.log(userToken);
-        setPageNum(pageNum);
-        setMaxPageNum(res.data.result.pageNumber);
-      });
-  }, []);
+				console.log(response);
+				console.log(click);
+			});
+	};
 
-  // useEffect(() => {
-  //   setCardList(dummy.results);
-  //   console.log(userToken);
-  // }, []);
+	const handleCardList = (data) => {
+		setCardList(data);
+	};
 
-  return (
-    <>
-      <Banner />
-      <Filtering getFilter={handleFilter} />
-      <div className={style.dataContainer}>
-        {cardList.map((item) => {
-          return <DataItem key={item.animalIdx} item={item} />;
-        })}
-      </div>
-      <PageButtons 
-        handleCardList={handleCardList} 
-        click={click} 
-        pageNum={pageNum} 
-        maxPageNum={maxPageNum}
-        data={data}
-      />
-    </>
-  );
+	useEffect(() => {
+		axios
+			.get(
+				'https://sjs.hana-umc.shop/animals',
+				{ params: { page: pageNum } },
+				{ withCredentials: true }
+			)
+			.then((res) => {
+				console.log(res.data.result);
+				setCardList(res.data.result.animal);
+				console.log(userToken);
+				setPageNum(pageNum);
+				setMaxPageNum(res.data.result.pageNumber);
+			});
+	}, []);
+
+	// useEffect(() => {
+	//   setCardList(dummy.results);
+	//   console.log(userToken);
+	// }, []);
+
+	return (
+		<>
+			<Banner />
+			<Filtering getFilter={handleFilter} />
+			<div className={style.dataContainer}>
+				{cardList.map((item) => {
+					return <DataItem key={item.animalIdx} item={item} />;
+				})}
+			</div>
+			<PageButtons
+				handleCardList={handleCardList}
+				click={click}
+				pageNum={pageNum}
+				maxPageNum={maxPageNum}
+				data={data}
+			/>
+		</>
+	);
 }
